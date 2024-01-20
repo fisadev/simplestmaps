41334d30d6f13612d6939cb19b836e02fe524799
Juan Pedro Fisanotti <fisadev@gmail.com>
2024-01-20 20:14:22 -0300

    Up version to publish

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="simplestmaps",
-    version="1.0.1",
+    version="1.0.2",
     description="Super simple on-liner maps in python.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
