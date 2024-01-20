7041406c4173bbb1c72f5d28d01f1d9349294504
Juan Pedro Fisanotti <fisadev@gmail.com>
2024-01-20 19:49:37 -0300

    Make it pip installable

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name="simplestmaps",
+    version="1.0.0",
+    description="Super simple on-liner maps in python.",
+    long_description=open("README.md").read(),
+    author="Juan Pedro Fisanotti",
+    author_email="fisadev@gmail.com",
+    url="http://github.com/fisadev/simplestmaps",
+    py_modules=["simplestmaps"],
+    license="MIT",
+    install_requires=["folium>=0.14.0"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+)
